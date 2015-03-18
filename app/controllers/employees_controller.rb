4bad5069da78faf841b69c9b5a791975009eae38
Jay Wengrow <jaywngrw@gmail.com>
2015-03-18 17:02:18 -0500

    namespace, versioning, and dealing with api key

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,7 +5,9 @@ class EmployeesController < ApplicationController
 
   def index
     # GOAL: @employees = Employee.all
-    employees_array = Unirest.get("http://localhost:3000/employees.json").body["employees"]
+    puts "ASDFADSF"
+    puts "#{ENV['API_BASE_URL']}"
+    employees_array = Unirest.get("#{ENV['API_BASE_URL']}/employees.json", :headers => {"X-User-Email" => ENV['API_EMAIL'], "Authorization" => "Token token=#{ENV['API_KEY']}"}).body["employees"]
     @employees = []
     employees_array.each do |employee_hash|
       @employees << Employee.new(employee_hash)
@@ -17,7 +19,7 @@ class EmployeesController < ApplicationController
   end
 
   def create
-    @employee = Unirest.post("http://localhost:3000/employees.json", :headers => {"Accept" => "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email => params[:email]}).body
+    @employee = Unirest.post("#{ENV['API_BASE_URL']}/employees.json", :headers => {"Accept" => "application/json"}, :parameters => {:first_name => params[:first_name], :last_name => params[:last_name], :email => params[:email]}).body
     redirect_to employee_path(@employee["id"])
   end
 end
